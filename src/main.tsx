a70b82b5962858a455750c5854bdcdc09637f463
Afreed Ahmad Abdul Rahuman <afreed.rahuman@cora-environment.com>
2026-04-29 10:19:50 +0800

    fix: finalize light mode theme and clean up CSS tokens

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import '@fontsource/geist-mono/300.css'
-import '@fontsource/geist-mono/400.css'
 import './index.css'
 import App from './App.tsx'
 
